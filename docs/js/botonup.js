5002e60a95a76a90e33b5673ca9f241e0223a126
Miguel Diaz <migueldiazyt32@gmail.com>
2025-08-29 22:03:38 -0500

    Mejora en el SEO de la pagina

---
+++ docs/js/botonup.js
@@ -0,0 +1,17 @@
+  const btnUp = document.getElementById("btnUp");
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      btnUp.style.display = "block";
+    } else {
+      btnUp.style.display = "none";
+    }
+  };
+  
+
+  btnUp.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
